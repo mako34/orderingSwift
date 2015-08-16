75378b0960f96ffe9dedb4839b749c17edbe80b7
TKT_SS9_43 <juanman234@gmail.com>
2015-08-16 18:46:59 +1000

    secondo

--- ShopOrders/Supplier.swift
+++ ShopOrders/Supplier.swift
@@ -12,7 +12,23 @@ import SwiftForms
 class Supplier: FormViewController {
     
     struct Static {
+        static let nameTag = "name"
+        static let passwordTag = "password"
+        static let lastNameTag = "lastName"
+        static let jobTag = "job"
+        static let emailTag = "email"
+        static let URLTag = "url"
+        static let phoneTag = "phone"
+        static let enabled = "enabled"
+        static let check = "check"
+        static let segmented = "segmented"
         static let picker = "picker"
+        static let birthday = "birthday"
+        static let categories = "categories"
+        static let button = "button"
+        static let stepper = "stepper"
+        static let slider = "slider"
+        static let textView = "textview"
     }
     
     required init(coder aDecoder: NSCoder) {
@@ -37,23 +53,28 @@ class Supplier: FormViewController {
         
         let section5 = FormSectionDescriptor()
         
-        var row = FormRowDescriptor(tag: Static.picker, rowType: .Picker, title: "Gender")
-        row.configuration[FormRowDescriptor.Configuration.Options] = ["F", "M", "U"]
-        row.configuration[FormRowDescriptor.Configuration.TitleFormatterClosure] = { value in
-            switch( value ) {
-            case "F":
-                return "Female"
-            case "M":
-                return "Male"
-            case "U":
-                return "I'd rather not to say"
-            default:
-                return nil
-            }
-            } as TitleFormatterClosure
+        var row = FormRowDescriptor(tag: Static.nameTag, rowType: .Name, title: "Name")
+        row.configuration[FormRowDescriptor.Configuration.CellConfiguration] = ["textField.placeholder" : "Supplier Name", "textField.textAlignment" : NSTextAlignment.Right.rawValue]
+        section5.addRow(row)
         
-        row.value = "M"
+        row = FormRowDescriptor(tag: Static.emailTag, rowType: .Email, title: "Email")
+        row.configuration[FormRowDescriptor.Configuration.CellConfiguration] = ["textField.placeholder" : "john@gmail.com", "textField.textAlignment" : NSTextAlignment.Right.rawValue]
+        section5.addRow(row)
         
+        
+        row = FormRowDescriptor(tag: Static.phoneTag, rowType: .Phone, title: "Phone")
+        row.configuration[FormRowDescriptor.Configuration.CellConfiguration] = ["textField.placeholder" : "e.g. 0034666777999", "textField.textAlignment" : NSTextAlignment.Right.rawValue]
+        section5.addRow(row)
+        
+        
+        row = FormRowDescriptor(tag: Static.button, rowType: .Button, title: "Products")
+        row.configuration[FormRowDescriptor.Configuration.DidSelectClosure] = {
+//            self.view.endEditing(true)
+
+            //segue
+            self.performSegueWithIdentifier("showProducts", sender: nil)
+            
+            } as DidSelectClosure
         section5.addRow(row)
         
         section5.headerTitle = "Date of order"
@@ -61,7 +82,7 @@ class Supplier: FormViewController {
         
         
         form.sections = [section5]
-        
+
         self.form = form
     }
     
