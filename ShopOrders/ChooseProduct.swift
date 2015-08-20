c8b68683098014d4fb1ee18764017c6acd498ae3
TKT_SS9_43 <juanman234@gmail.com>
2015-08-20 20:37:01 +1000

    show products on products selector

--- ShopOrders/ChooseProduct.swift
+++ ShopOrders/ChooseProduct.swift
@@ -7,13 +7,15 @@
 //
 
 import Foundation
+import Realm
 import UIKit
 
 //, UITableViewDataSource, UITableViewDelegate, UISearchDisplayDelegate, UISearchBarDelegate
-class ChooseProduct: UIViewController
+class ChooseProduct: UIViewController, UITableViewDelegate, UITableViewDataSource
 {
     
-
+    var Products : RLMResults!
+    
     @IBOutlet weak var tableView: UITableView!
     
     @IBAction func cancelButtonPressed(sender: AnyObject) {
@@ -22,133 +24,64 @@ class ChooseProduct: UIViewController
     }
     
     
-//    var friendsArray = [FriendItem]()
-//    var filteredFriends = [FriendItem]()
-//    
-//    
-//    
-//    override func viewDidLoad()
-//    {
-//        super.viewDidLoad()
-//        
-//        
-//        
-//        self.friendsArray += [FriendItem(name: "Vea Software")]
-//        self.friendsArray += [FriendItem(name: "Apple")]
-//        self.friendsArray += [FriendItem(name: "iTunes")]
-//        self.friendsArray += [FriendItem(name: "iPhone")]
-//        self.friendsArray += [FriendItem(name: "Mac")]
-//        
-//        self.tableView.reloadData()
-//        
-//    }
-//    
-//
-//    
-//    override func didReceiveMemoryWarning()
-//    {
-//        super.didReceiveMemoryWarning()
-//        // Dispose of any resources that can be recreated.
-//    }
-//    
-//    
-//    // MARK: - Table View
-//    
-//    func numberOfSectionsInTableView(tableView: UITableView) -> Int
-//    {
-//        return 1
-//    }
-//    
-//    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
-//    {
-//        if (tableView == self.searchDisplayController?.searchResultsTableView)
-//        {
-//            return self.filteredFriends.count
-//        }
-//        else
-//        {
-//            return self.friendsArray.count
-//        }
-//    }
-//    
-//    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
-//    {
-//        
-//        let cell = self.tableView.dequeueReusableCellWithIdentifier("cell") as! UITableViewCell
-//        
-//        var friend : FriendItem
-//        
-//        if (tableView == self.searchDisplayController?.searchResultsTableView)
-//        {
-//            friend = self.filteredFriends[indexPath.row]
-//        }
-//        else
-//        {
-//            friend = self.friendsArray[indexPath.row]
-//        }
-//        
-//        cell.textLabel?.text = friend.name
-//        
-//        return cell
-//        
-//    }
-//    
-//    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath)
-//    {
-//        tableView.deselectRowAtIndexPath(indexPath, animated: true)
-//        
-//        var friend : FriendItem
-//        
-//        if (tableView == self.searchDisplayController?.searchResultsTableView)
-//        {
-//            friend = self.filteredFriends[indexPath.row]
-//        }
-//        else
-//        {
-//            friend = self.friendsArray[indexPath.row]
-//        }
-//        
-//        println(friend.name)
-//        
-//        
-//    }
-//    
-//    // MARK: - Search Methods
-//    
-//    func filterContenctsForSearchText(searchText: String, scope: String = "Title")
-//    {
-//        
-//        self.filteredFriends = self.friendsArray.filter({( friend : FriendItem) -> Bool in
-//            
-//            var categoryMatch = (scope == "Title")
-//            var stringMatch = friend.name.rangeOfString(searchText)
-//            
-//            return categoryMatch && (stringMatch != nil)
-//            
-//        })
-//        
-//        
-//    }
-//    
-//    func searchDisplayController(controller: UISearchDisplayController, shouldReloadTableForSearchString searchString: String!) -> Bool
-//    {
-//        
-//        self.filterContenctsForSearchText(searchString, scope: "Title")
-//        
-//        return true
-//        
-//        
-//    }
-//    
-//    
-//    func searchDisplayController(controller: UISearchDisplayController, shouldReloadTableForSearchScope searchOption: Int) -> Bool
-//    {
-//        
-//        self.filterContenctsForSearchText(self.searchDisplayController!.searchBar.text, scope: "Title")
-//        
-//        return true
-//        
-//    }
+    override func viewDidLoad() {
+        //get all prods!
+     
+        var products : RLMResults {
+            get { //q es el get?
+                return ProductsDao.allObjects().sortedResultsUsingProperty("name", ascending: true)
+            }
+        }
+        
+        Products = products
+        
+        println("tus prods :: \(Products)")
+
+        
+    }
+
+    // MARK: - Table view data source
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return Int(self.Products.count)
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! UITableViewCell
+        
+        // Configure the cell...
+        
+        
+        let index = UInt(indexPath.row)
+        let productItem = self.Products.objectAtIndex(index) as! ProductsDao
+        
+        cell.textLabel?.text = productItem.name
+        
+        return cell
+    }
+    
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        tableView .deselectRowAtIndexPath(indexPath, animated: true)
+        
+        let index = UInt(indexPath.row)
+        let productItem = self.Products.objectAtIndex(index) as! ProductsDao
+        
+        
+        println("selecto \(productItem)")
+        
+        //            self.performSegueWithIdentifier("showSupplier", sender: nil)
+    }
     
     
 }
